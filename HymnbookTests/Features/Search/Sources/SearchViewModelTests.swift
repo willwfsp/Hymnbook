af7427c33f536b11d74c2181b82117718f98f19b
Will Policiano <w.policiano@gmail.com>
2023-07-01 15:40:37 -0500

    Use Combine instead of swift concurrency

--- HymnbookTests/Features/Search/Sources/SearchViewModelTests.swift
+++ HymnbookTests/Features/Search/Sources/SearchViewModelTests.swift
@@ -9,21 +9,24 @@ enum SearchState {
     case empty
 }
 
+typealias FetchRecentSearches = () -> AnyPublisher<[String], Never>
+
 final class SearchViewModel: ObservableObject {
     @Published var state = SearchState.idle
 
-    private let fetchRecentSearches: () async -> [String]
+    private let fetchRecentSearches: FetchRecentSearches
 
     init(
-        fetchRecentSearches: @escaping () async -> [String]
+        fetchRecentSearches: @escaping FetchRecentSearches
     ) {
         self.fetchRecentSearches = fetchRecentSearches
     }
 
-    func onAppear() async {
+    func onAppear() {
         state = .loading
-        _ = await fetchRecentSearches()
-        state = .empty
+        fetchRecentSearches().sink { _ in
+            self.state = .empty
+        }
     }
 }
 
@@ -31,52 +34,27 @@ final class SearchViewModelTests: XCTestCase {
     func test_initsIdling() {
         let (_, spy) = makeSUT()
 
+        XCTAssertTrue(spy.events.isEmpty)
+    }
+
+    func test_awaysFetchRecentSearchesOnAppear() {
+        let (sut, spy) = makeSUT()
+
+        sut.onAppear()
+        sut.onAppear()
+
         XCTAssertEqual(spy.events, [
-            .state(.idle)
+            .fetchRecentSearches,
+            .fetchRecentSearches
         ])
     }
 
-    func test_fetchesNoRecentSearchOnAppear() async {
-        let (sut, spy) = makeSUT(expectedRecentSearches: [])
-
-        await sut.onAppear()
-
-        XCTAssertEqual(spy.events, [
-            .state(.idle),
-            .state(.loading),
-            .fetchRecentSearches,
-            .state(.empty)
-        ])
-    }
-
-    func test_awaysLoadOnAppear() async {
-        let (sut, spy) = makeSUT(expectedRecentSearches: [])
-
-        await sut.onAppear()
-        await sut.onAppear()
-
-        XCTAssertEqual(spy.events, [
-            .state(.idle),
-            .state(.loading),
-            .fetchRecentSearches,
-            .state(.empty),
-
-            .state(.loading),
-            .fetchRecentSearches,
-            .state(.empty)
-        ])
-    }
-
-    private func makeSUT(
-        expectedRecentSearches _: [String] = []
-    ) -> (
+    private func makeSUT() -> (
         SearchViewModel,
         SearchViewModelDependenciesSpy
     ) {
-        let spy = SearchViewModelDependenciesSpy(recentSearches: [])
+        let spy = SearchViewModelDependenciesSpy()
         let sut = SearchViewModel(fetchRecentSearches: spy.fetchRecentSearches)
-
-        spy.bind(viewModel: sut)
 
         return (sut, spy)
     }
@@ -87,27 +65,22 @@ extension SearchState: AutoEquatable {}
 final class SearchViewModelDependenciesSpy {
     enum Event: Equatable {
         case fetchRecentSearches
-        case state(SearchState)
     }
 
     private(set) var events = [Event]()
-    private var cancellables = [AnyCancellable]()
+    private var promises = [(Result<[String], Never>) -> Void]()
 
-    private let recentSearches: [String]
-
-    init(recentSearches: [String]) {
-        self.recentSearches = recentSearches
-    }
-
-    func bind(viewModel: SearchViewModel) {
-        viewModel.$state.sink { state in
-            self.events.append(.state(state))
-        }.store(in: &cancellables)
-    }
-
-    func fetchRecentSearches() async -> [String] {
+    func fetchRecentSearches() -> AnyPublisher<[String], Never> {
         events.append(.fetchRecentSearches)
 
-        return recentSearches
+        return Deferred {
+            Future { promise in
+                self.promises.append(promise)
+            }
+        }.eraseToAnyPublisher()
+    }
+
+    func complete(with recentSearches: [String], at index: Int = 0) {
+        promises[index](.success(recentSearches))
     }
 }
